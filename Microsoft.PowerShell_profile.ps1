556d1699848f7ec5a98cb897ce9fd9a9fde52f9d
Kris harper <kris.p.kharper@gmail.com>
2012-12-20 16:25:31 -0500

    Customized my prompt

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,4 @@
-$(Get-PSProvider FileSystem).Home = '\\nyprodfs01\profiles$\kharper'
+$(Get-PSProvider FileSystem).Home = $env:Home
 New-Alias which get-command
 New-Alias np "C:\Program Files (x86)\Notepad++\notepad++.exe"
 New-Alias vim "C:\Program Files (x86)\Vim\vim73\vim.exe"
@@ -9,3 +9,45 @@ New-Alias putty "C:\Program Files (x86)\Putty\putty.exe"
 
 Remove-Item alias:cd
 Set-Alias cd '\\nyprodfs01\profiles$\kharper\My Documents\WindowsPowerShell\cd.ps1'
+
+# Set up prompt including the posh-git module
+Push-Location (Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)
+Import-Module posh-git
+
+function Shorten-Path([string] $path) {
+   $loc = $path.Replace($env:home, '~')
+   # remove prefix for UNC paths
+   $loc = $loc -replace '^[^:]+::', ''
+   # make path shorter like tabs in Vim,
+   # handle paths starting with \\ and . correctly
+   return ($loc -replace '\\(\.?)([^\\])[^\\]*(?=\\)','\$1$2')
+}
+
+# Set up a simple prompt, adding the git prompt parts inside git repos
+function prompt {
+    $provider = (pwd).Provider.Name
+
+    if ($provider -eq "FileSystem") {
+        $realLASTEXITCODE = $LASTEXITCODE
+
+        # Reset color, which can be messed up by Enable-GitColors
+        $Host.UI.RawUI.ForegroundColor = $GitPromptSettings.DefaultForegroundColor
+    }
+    
+    $green = [ConsoleColor]::Green
+    $cyan = [ConsoleColor]::Cyan
+
+    Write-Host ("[" + ([Net.Dns]::GetHostName()) + "] ") -n -f $green
+    Write-Host ("<" + (Shorten-Path (pwd).Path) + ">") -n -f $cyan
+
+    if ($provider -eq "FileSystem") {
+        Write-VcsStatus
+
+        $global:LASTEXITCODE = $realLASTEXITCODE
+    }
+    return "$ "
+}
+
+Enable-GitColors
+
+Pop-Location
