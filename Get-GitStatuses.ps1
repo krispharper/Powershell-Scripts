af62778c8e0daf86e980322791a15d2e229cf64c
Kris Harper <kris.p.harper@gmail.com>
2016-09-12 10:46:26 -0400

    Updated to just use top-level directories

--- Get-GitStatuses.ps1
+++ Get-GitStatuses.ps1
@@ -7,19 +7,20 @@ The directory root which holds all the git repositories to be checked
 #>
 
 param (
-    [string] $rootPath = "~"
+    [string] $rootPath = "."
 )
 
 Push-Location -StackName s .
 
-ls $rootPath -Attributes Directory -Force -Name -R -Filter .git |% {
-    cd (Join-Path -Path $rootPath -ChildPath $_)
+ls $rootPath -Attributes Directory -Force -Depth 1 -Filter .git |% {
+    cd $_.FullName
     cd ..
     $status = git status -s
 
     if ($status) {
-        $_
+        $_.FullName
         git status -s
+        ""
     }
 }
 
